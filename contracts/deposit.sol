19ab865d535830b04492ba75cc29892f752364d6
Boris Polania <boris.polania@gmail.com>
2022-04-05 19:28:17 -0700

    Debugging Session

--- contracts/deposit.sol
+++ contracts/deposit.sol
@@ -2,12 +2,13 @@
 pragma solidity ^0.8.0;
 
 import '@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol';
+import '@openzeppelin/contracts/access/Ownable.sol';
 import './ubONE.sol';
 
-contract Deposit {
+contract Deposit is Ownable {
 
-    address public ubONEAddress;
-    address public oneAnchor;
+    address private ubONEAddress;
+    address private oneAnchor;
 
     uint public depositedAmount;
     int public exchangeRate;
@@ -17,7 +18,6 @@ contract Deposit {
     constructor() {
         priceFeed = AggregatorV3Interface(0xcEe686F89bc0dABAd95AEAAC980aE1d97A075FAD);
         oneAnchor = 0x8Dc67adCeCC140E12E838D185FDF4ebc2979B365;
-        ubONEAddress = 0x8Dc67adCeCC140E12E838D185FDF4ebc2979B365;
     }
 
     /*
@@ -63,5 +63,13 @@ contract Deposit {
         // This is the current recommended method to use.
         (bool sent, ) = _to.call{value: msg.value}("");
         require(sent, "Failed to send ONE");
+    }
+
+    function setubONEAddress(address a) public onlyOwner {
+        ubONEAddress = a;
+    }
+
+    function getubONEAddress() public virtual onlyOwner returns (address) {
+        return ubONEAddress;
     }
 }
