98ac676c679eb7f599e3d3a1b67a55cde6009112
Boris Polania <boris.polania@gmail.com>
2022-04-12 13:46:40 -0700

    Roles Added

--- contracts/OneAnchor.sol
+++ contracts/OneAnchor.sol
@@ -4,14 +4,14 @@ pragma solidity ^0.8.0;
 import '@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol';
 import '@openzeppelin/contracts/access/Ownable.sol';
 import '@openzeppelin/contracts/utils/math/SafeMath.sol';
-import '@openzeppelin/contracts/utils/Context.sol';
+import '@openzeppelin/contracts/access/AccessControl.sol';
 import './interfaces/IUniswapV2Router02.sol';
 import './interfaces/IWrappedUST.sol';
 import './interfaces/IWrappedaUST.sol';
 import './interfaces/ISushiSwapLPToken.sol';
 import './Reserves.sol';
 
-contract OneAnchor is Context, Ownable  {
+contract OneAnchor is  Ownable, AccessControl  {
 
     using SafeMath for uint;
 
@@ -38,6 +38,8 @@ contract OneAnchor is Context, Ownable  {
     uint[] deposits;
     uint[] withdrawals;
 
+    bytes32 public constant CLEARING_ROLE = keccak256("CLEARING_ROLE");
+
     constructor() {
         // addresses
         clONEUSD = 0xcEe686F89bc0dABAd95AEAAC980aE1d97A075FAD;
@@ -46,7 +48,7 @@ contract OneAnchor is Context, Ownable  {
         wUST = 0x224e64ec1BDce3870a6a6c777eDd450454068FEC;
         wONE = 0xcF664087a5bB0237a0BAd6742852ec6c8d69A27a;
         sushiSwapLPToken = 0x4dABF6C57A8beA012F1EAa1259Ceed2a62AC7df2;
-        //contracts
+        // contracts
         priceFeedOneUsd = AggregatorV3Interface(clONEUSD);
         priceFeedUstaUst = AggregatorV3Interface(clUSTaUST);
         router = IUniswapV2Router02(uniswapV2Router02);
@@ -68,19 +70,28 @@ contract OneAnchor is Context, Ownable  {
      */
     function deposit() public payable {
         uint value = msg.value;
+        // Get UST Reserves in LP
         (uint USTReserves,,) = lpToken.getReserves();
         require(USTReserves > 0, "There are not enough UST reserves in the Liquidity Pool");
+        // Get the amount of UST being deposited and check that 
+        // there are enough reserves to clear the transacion
         uint OneAmountInUST = uint(getExchangeRate(priceFeedOneUsd)).mul(value);
         require(OneAmountInUST < USTReserves, "There are not enough UST reserves in the Liquidity Pool");
+        // Swap ONE for wrapped UST in Sushi
         uint[] memory finalValues = router.swapExactETHForTokens{value: value}(1000, path, reserves, block.timestamp + 120 seconds);
         uint finalUSTValue = finalValues[1];
         require(finalUSTValue > 0, "ONE/UST Failed");
+        // Calculate the number of aUST that must be sent to the user
+        // to the user and send them to it 
         uint USTAmountInaUST = uint(getExchangeRate(priceFeedUstaUst)).mul(finalUSTValue);
         bool didPay = pay(msg.sender, uint(USTAmountInaUST), 0);
         require(didPay == true, "aUST were not transfer to the user");
+        // move the USTs to the reserves contract
         bool didTransfer = wust.transferFrom(address(this), reserves, finalUSTValue);
         require(didTransfer == true, "Deposited amount could not be transfered to reserves");
+        // add the deposited amount to th stake queue
         stake(finalUSTValue);
+        // emit the event
         emit Deposit(msg.sender, msg.value, USTReserves);
     }
     /*
@@ -89,15 +100,21 @@ contract OneAnchor is Context, Ownable  {
      */
     function withdrawal(uint amount) public payable {
         require(amount > 0, "Withdrawal amount must be greater than 0");
+        // Get aUST Reserves in LP
         (,uint aUSTReserves,) = lpToken.getReserves();
         require(aUSTReserves > 0, "There are not enough aUST reserves in the Liquidity Pool");
+        // Calculate the number of ONEs that must be sent to user
+        // and send them to it
         uint aUSTAmountInUST = uint(getExchangeRate(priceFeedUstaUst)).div(amount);
         uint aUSTAmountInONE = uint(getExchangeRate(priceFeedOneUsd)).div(aUSTAmountInUST);
         bool didPay = pay(msg.sender, uint(aUSTAmountInONE), 1);
         require(didPay == true, "ONE were not transfer to the user");
+        // move the aUSTs to the reserves contract
         bool didTransfer = waust.transferFrom(address(this), reserves, amount);
         require(didTransfer == true, "Deposited amount could not be transfered to reserves");
+        // add the deposited amount to th stake queue
         unstake(amount);
+        // emit the event
         emit Withdrawal(msg.sender, msg.value, aUSTAmountInUST);
     }
     /*
@@ -127,12 +144,10 @@ contract OneAnchor is Context, Ownable  {
             return false;
         }
     }
-    
-
     /**
      * Returns Pair exchange rate
      */
-    function getExchangeRate(AggregatorV3Interface cl) public view returns (int) {
+    function getExchangeRate(AggregatorV3Interface cl) internal view returns (int) {
         (
             , 
             int price,
@@ -142,17 +157,48 @@ contract OneAnchor is Context, Ownable  {
         ) = cl.latestRoundData();
         return price;
     }
-
-    function dequeue(uint action) external {
-
+    /**
+     * get the value of the next transaction in the queue
+     */
+    function getNextTransaction(uint action) external view returns (uint) {
+        require(hasRole(CLEARING_ROLE, msg.sender), "Caller is not an admin");
+        if (action == 0){
+            return deposits[0];
+        } else if (action == 1){
+            return withdrawals[0];
+        } else {
+            return 0;
+        }
+    }
+    /**
+     * clear the next transaction in the queue
+     */
+    function clearNextTransaction(uint action) external {
+        require(hasRole(CLEARING_ROLE, msg.sender), "Caller is not an admin");
+        if (action == 0){
+            reserve.withdrawUST(deposits[0]);
+            
+        } else if (action == 1){
+            reserve.withdrawaUST(deposits[0]);
+        } 
+        dequeue(action);
+    }
+    /**
+     * remove a transaction fro the queue
+     */
+    function dequeue(uint action) internal {
         if (action == 0){
             delete deposits[0];
         }
-
         if (action == 1){
             delete withdrawals[0];
-        }
-        
+        } 
+    }
+    /**
+     * set the clearing role (intended for reserves contract)
+     */
+    function setClearingRole(address admin) external onlyOwner {
+        _setupRole(CLEARING_ROLE, admin);
     }
 
 }
--- contracts/Reserves.sol
+++ contracts/Reserves.sol
@@ -3,47 +3,120 @@ pragma solidity ^0.8.0;
 
 import '@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol';
 import '@openzeppelin/contracts/access/Ownable.sol';
-import '@openzeppelin/contracts/utils/Context.sol';
+import '@openzeppelin/contracts/access/AccessControl.sol';
 import './interfaces/IWrappedaUST.sol';
 
 
-contract Reserves is Context, Ownable {
+contract Reserves is Ownable, AccessControl {
+
+    bytes32 public constant OWNER_ROLE = keccak256("OWNER_ROLE");
+    bytes32 public constant EARN_ROLE = keccak256("EARN_ROLE");
 
     IWrappedaUST internal waust;
 
     uint private USTBalance;
     uint private aUSTBalance;
+    uint private ONEBalance;
 
-    address private UST;
-    address private aUST;
-
-    int public exchangeRate;
-
+    address private earnAccount;
     
     constructor() {
         USTBalance = 0;
         aUSTBalance = 0;
+        ONEBalance = 0;
     }
-
-    function addToUSTReserve(uint amount) external view {}
-
-    function removeFromUSTReserve() external view {}
-
+    /*
+     * Modify Reserve amounts.
+     * Add or remove from the different assets balances
+     */
+    function addToUSTReserve(uint amount) internal {
+        USTBalance += amount;
+    }
+    function removeFromUSTReserve(uint amount) internal {
+        USTBalance -= amount;
+    }
+    function addToaUSTReserve(uint amount) internal {
+        aUSTBalance += amount;
+    }
+    function removeFromaUSTReserve(uint amount) internal {
+        aUSTBalance -= amount;
+    }
+    function addToONEReserve(uint amount) internal {
+        ONEBalance += amount;
+    }
+    function removeFromONEReserve(uint amount) internal {
+        ONEBalance -= amount;
+    }
+    /*
+     * Get Reserve amounts.
+     * Get the balance of the different assets balances
+     */
     function getUSTBalance() external view returns (uint) {
+        require(hasRole(OWNER_ROLE, msg.sender), "Caller does not have access to this function");
         return USTBalance;
     }
-
+    function getaUSTBalance() external view returns (uint) {
+        require(hasRole(OWNER_ROLE, msg.sender), "Caller does not have access to this function");
+        return aUSTBalance;
+    }
+    function getONEBalance() external view returns (uint) {
+        require(hasRole(OWNER_ROLE, msg.sender), "Caller does not have access to this function");
+        return ONEBalance;
+    }
+    /*
+     * Pay users.
+     * Send assets to users
+     */
     function payaUST(address to, uint amount) external returns (bool) {
-        return waust.transferFrom(address(this), to, amount);
+        require(hasRole(OWNER_ROLE, msg.sender), "Caller does not own these reserves");
+        bool didTransfer = waust.transferFrom(address(this), to, amount);
+        require(didTransfer == true, "Payment failed");
+        removeFromaUSTReserve(amount);
+        return didTransfer;
+
     }
     function payONE(address to, uint amount) external returns (bool)  {
-        return sendViaCall(payable(to), amount);
+        require(hasRole(OWNER_ROLE, msg.sender), "Caller does not own these reserves");
+        bool didTransfer = sendViaCall(payable(to), amount);
+        require(didTransfer == true, "Payment failed");
+        removeFromONEReserve(amount);
+        return didTransfer;
     }
-
+    /*
+     * Pay users.
+     * Send assets to users
+     */
+    function withdrawaUST(uint amount) external returns (bool) {
+        require(hasRole(OWNER_ROLE, msg.sender), "Caller does not own these reserves");
+        bool didTransfer = waust.transferFrom(address(this), earnAccount, amount);
+        require(didTransfer == true, "Transfer failed");
+        removeFromaUSTReserve(amount);
+        return didTransfer;
+    }
+    function withdrawUST(uint amount) external returns (bool) {
+        require(hasRole(OWNER_ROLE, msg.sender), "Caller does not own these reserves");
+        bool didTransfer = waust.transferFrom(address(this), earnAccount, amount);
+        require(didTransfer == true, "Transfer failed");
+        removeFromaUSTReserve(amount);
+        return didTransfer;
+    }
+    /*
+     * Send ONEs to address.
+     */
     function sendViaCall(address payable _to, uint256 amount) internal returns (bool) {
         (bool sent, ) = _to.call{value: amount}("");
         return sent;
     }
-
-
+    /**
+     * set the account that will deposit into Anchor Earn
+     */
+    function setEarnAddress(address account) external onlyOwner {
+        earnAccount = account;
+    }
+    /**
+     * set the owner role (intended for oneAnchor contract)
+     */
+    function setOnwerRole(address owner) external onlyOwner {
+        _setupRole(OWNER_ROLE, owner);
+    }
 }
