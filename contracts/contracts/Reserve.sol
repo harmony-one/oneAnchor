a525f702b35ece046bfb9d9c6680c47fbf3f48e6
DakBuk <brucdarc@gmail.com>
2022-04-20 18:30:30 -0600

    Add some comments to reserve, also remove state variable declarations since they were moved

--- contracts/contracts/Reserve.sol
+++ contracts/contracts/Reserve.sol
@@ -1,26 +1,39 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.13;
 
-import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/interfaces/IERC20Upgradeable.sol";
+import "./ReserveDeclarations.sol";
 
-contract Reserve is OwnableUpgradeable, AccessControlUpgradeable {
-    bytes32 public constant OPERATOR_ROLE = keccak256("OPERATOR_ROLE");
-    bytes32 public constant EARN_ROLE = keccak256("EARN_ROLE");
+/*
+    So what we essentially want is a service that will migrate UST to tera and
+    return with aUST, and the opposite, take aUST from harmony, bridge to tera and return
+    with UST.
 
-    IERC20Upgradeable wAUST;
-    IERC20Upgradeable wUST;
+    2 ways to accomplish this
+    1. individual model, operators move funds equal to users requests, then return later with
+        the funds and the tokens get transferred to the users.
+
+    2. Some sort of liquidity model, where there are UST and aUST in the pool, and
+        the operators go back and forth as the ratio changes
+        this is basically the same as a normal dex liquidity pool with incentives, so
+        this probably is not what we need here since that already exists
+
+    So then with #1, we need to keep track of all desired orders and track when and how
+    they get furfilled.
+
+    So then control flow could work like this : Operator takes a batch of requests,
+    takes either UST or aUST, migrates to tera, then returns with the other asset
+
+    We could even build in an internal order book system to just settle people who want
+    UST with aUST and vice versa with each other, provided sufficient volume
+        -Maybe just leave that to the dexes
+
+*/
 
 
-    uint256 public USTBalance;
-    uint256 public aUSTBalance;
-    uint256 public ONEBalance;
-
-    mapping(address => int256) private balances;
-
-    address private earnAccount;
+contract Reserve is AccessControlUpgradeable, ReserveDeclarations {
 
     modifier onlyOperator() {
         require(
@@ -30,16 +43,11 @@ contract Reserve is OwnableUpgradeable, AccessControlUpgradeable {
         _;
     }
 
-
     constructor(address _wAUST, address _wUST)
     {
+        __Ownable_init();
         wAUST = IERC20Upgradeable(_wAUST);
         wUST = IERC20Upgradeable(_wUST);
-    }
-
-
-    function __Reserve_init() external onlyInitializing {
-        __Ownable_init();
     }
 
     /*
@@ -98,8 +106,8 @@ contract Reserve is OwnableUpgradeable, AccessControlUpgradeable {
             msg.sender,
             amount
         );
+        require(didTransfer == true, "Transfer failed");
 
-        require(didTransfer == true, "Payment failed");
         removeFromUSTReserve(amount);
         return didTransfer;
     }
