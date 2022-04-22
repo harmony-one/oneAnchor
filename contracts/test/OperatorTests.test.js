8bfedc229a80c1044d835e4c4bdec026d8643418
DakBuk <brucdarc@gmail.com>
2022-04-22 16:54:39 -0600

    test cases template

---
+++ contracts/test/OperatorTests.test.js
@@ -0,0 +1,22 @@
+const OneAnchor = artifacts.require("OneAnchor");
+const { BN, expectRevert, time } = require('@openzeppelin/test-helpers');
+
+contract("Operator Tests", async accounts => {
+
+    before(async () => {
+
+    });
+
+    beforeEach(async () =>{
+
+    });
+
+    describe("Operator Tests", () => {
+
+        it("Swap UST for AUST", async () => {
+
+        });
+
+    });
+
+});
---
+++ contracts/test/SwapTests.test.js
@@ -0,0 +1,34 @@
+const OneAnchor = artifacts.require("OneAnchor");
+const { BN, expectRevert, time } = require('@openzeppelin/test-helpers');
+
+contract("Swap Tests", async accounts => {
+
+    before(async () => {
+
+    });
+
+    beforeEach(async () =>{
+
+    });
+
+    describe("Swap Tests", () => {
+
+        it("Swap UST for AUST", async () => {
+
+        });
+
+        it("Swap AUST for UST", async () => {
+
+        });
+
+        it("Deposit (Sushiswap + Internal Swap)", async () => {
+
+        });
+
+        it("Withdraw (Sushiswap + Internal Swap)", async () => {
+
+        });
+
+    });
+
+});
