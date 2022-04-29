4ccdefc9451702a054c2db682bd06904f8200b14
Boris Polania <boris.polania@gmail.com>
2022-04-29 01:16:56 -0700

    Web-app removed
    Now web-app has its own repo https://github.com/harmony-one/oneAnchor-webapp

--- web-app/src/redux/root-reducer.ts
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-
-import toastReducer from "./toast/toast.reducer";
-
-export const rootReducer = combineReducers({
-  toast: toastReducer
-});
-
-export type RootState = ReturnType<typeof rootReducer>;
